b4fe239b5089bb8d4b6e62ed7f37783ae8a05cd9
Richardson Milton <richardson.milton@my.tccd.edu>
2019-11-23 22:33:16 +0000

    added factorial to this code.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-
+// Richardson Gage Milton
+// Dr. T Class Unit 8 
+// 2019-11-23 - 
 // C++ recursive function to  
 // solve tower of hanoi puzzle  
 #include <bits/stdc++.h> 
+#include<vector>
+#include<iostream> 
 using namespace std; 
   
 void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)  
@@ -17,13 +21,43 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
                                 " to rod " << to_rod << endl;  
     towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);  
 }  
-  
+
+
+using namespace std; 
+
+// function to find factorial of given number 
+unsigned int factorial(unsigned int n) 
+{ 
+	if (n == 0) 
+	return 1; 
+	return n * factorial(n - 1); 
+}
 // Driver code 
 int main()  
 {  
-    int n = 4; // Number of disks  
+    int n = 5; // Number of disks  
     towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods  
+
+// Make a new line for the recursive function 
+    cout << "\n\n" << endl; 
+
+/*Write a recursive function factorialR that will generate factorials based upon the user starting point (validated) input from the keyboard. i.e. When the user enters, 4, the function should multiply 4 x 3 x 2 x 1, and output the result of 24. */
+
+    	int num = 4; 
+      cout << "Enter a Number" << endl; 
+      cin >> num; 
+
+    
+	cout << "Factorial of " << num << " is " << factorial(num) << endl; 
+	return 0; 
     return 0;  
 }  
   
-// This is code is contributed by rathbhupendra 
+// Some of this is code is contributed by rathbhupendra and Shivi_Aggarwal 
+
+// C++ program to find factorial of given number 
+
+
+
+
+
